b673d91b80e6b3dd542fb80e603106e32ca79623
jpc <jean.paul.ik@gmail.com>
2012-06-28 16:36:32 +0200

    build dependencies sbt in parent project

--- project/Build.scala
+++ project/Build.scala
@@ -2,12 +2,15 @@ import sbt._
 import Keys._
 
 object HelloBuild extends Build {
-    lazy val root = Project(id = "morph-parent",
-                            base = file(".")) aggregate(core, querygen)
+  lazy val root = Project(id = "morph-parent",
+                          base = file(".")) aggregate(core, querygen,r2rmlTc)
 
-    lazy val core = Project(id = "morph-core",
-                           base = file("morph-core"))
+  lazy val core = Project(id = "morph-core",
+                          base = file("morph-core"))
 
-    lazy val querygen = Project(id = "morph-querygen",
-                           base = file("morph-querygen"))
+  lazy val querygen = Project(id = "morph-querygen",
+                              base = file("morph-querygen")) dependsOn(core)
+
+  lazy val r2rmlTc = Project(id = "morph-r2rml-tc",
+                             base = file("morph-r2rml-tc")) dependsOn(querygen)
 }
